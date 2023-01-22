d9f85962eea4a30e5ed86eece26699b24930d099
Karla Oshikawa <karla_hara@hotmail.com>
2023-01-23 05:56:49 +0900

    NLW setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log("Karla");
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server running!')
+})
